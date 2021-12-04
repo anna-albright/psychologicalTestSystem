0ae5d09ac652a45851046897c14db01d5c4c141b
samngn2021 <samngn2021@gmail.com>
2021-12-04 14:35:34 -0800

    Added Read class and main class. Also fixed .txt file format

---
+++ PsychTest/src/MainClass.java
@@ -0,0 +1,14 @@
+import java.io.File;
+public class MainClass {
+    public static void main(String[] args) {
+
+        //Note that the file reader is already pre set to read files within the "data" folder :)
+
+        //converts entire file to an array
+        Read.fileToArray("consentForm.txt");
+
+        //prints entire file contents to the console for our convenience
+        Read.previewFileContents("surveys/PERSONALITY - 16 Personality Factors.txt");
+
+    }
+}
