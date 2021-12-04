a810791a321b77180b9eaf8a5d97885113095553
samngn2021 <samngn2021@gmail.com>
2021-12-04 12:39:07 -0800

    created Survey and Statistics class, added data folder

---
+++ PsychTest/src/Survey.java
@@ -0,0 +1,36 @@
+public class Survey {
+    String categoryName;
+    String surveyName;
+    boolean consentChoice;
+
+    public void select()
+    {
+
+    }
+    public void displayConsent()
+    {
+
+
+    }
+    public void takeSurvey()
+    {
+        //will need a variable to store user answers
+
+    }
+    public void showResult()
+    {
+
+    }
+
+// will implement observer stuff later
+/*    public void setState(){
+
+    }
+    public  getState(){
+
+    }*/
+  /*  public void notifyObservers()
+    {
+
+    }*/
+}
